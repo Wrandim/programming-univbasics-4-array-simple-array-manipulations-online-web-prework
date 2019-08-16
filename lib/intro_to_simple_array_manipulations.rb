cf041127a1f559deff4841e01f1609ec64b88c57
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-16 21:09:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -65,7 +65,7 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
  
  #using_delete
  
- def using_delete(instructors, ("Steven"))
+ def using_delete(instructors, Steven)
    no_offense_steven = instructors.delete("Steven")
  end
  
